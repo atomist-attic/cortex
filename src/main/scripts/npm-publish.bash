50b60c6809a490ae11523427bfb78918dc61e461
David Dooling <david@atomist.com>
2017-04-10 21:25:17 -0500

    Master and release builds working
    Fix name of cortex in POM and package.json.  Use property replacement
    to create package.json with proper versions of cortex and rug.  Rework
    phases of some steps to make sure everything gets done in order.  Use
    conditional in POM that keys of command-line property setting to
    download proper @atomist/rug version.
    
    Clean up npm-publish script, which no longer has to download
    dependencies.
    
    Clean up Travis CI build script.  Install node/npm at install step,
    making it available for build and avoiding the default Java install
    step.
    
    Move TypeScript code up a few directories under src.  No need for it
    to be so nested and it prevents GitHub from automatically displaying
    diffs.
    
    Make all node/npm versions the same.  Update cortex version in POM.

--- src/main/scripts/npm-publish.bash
+++ src/main/scripts/npm-publish.bash
@@ -26,24 +26,18 @@ function publish() {
     if [[ ! $module_name ]]; then
         err "publish: missing required parameter: MODULE_NAME"
         return 10
-    else
-        shift
     fi
+    shift
 
     local target="target/.atomist/node_modules/@atomist/$module_name"
-    local package="$target/package.json"
-    if ! sed "s/REPLACE_ME/$module_version/g" "$package.in" > "$package"; then
-        err "failed to set version in $package"
-        return 1
-    fi
-    rm -f "$package.in"
 
     # npm honors this
     rm -f "$target/.gitignore"
 
+    local registry
     if [[ $module_version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
         if [[ $NPM_TOKEN ]]; then
-            msg "Creating local .npmrc using API key from environment"
+            msg "creating local .npmrc using NPM token from environment"
             if ! ( umask 077 && echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > "$HOME/.npmrc" ); then
                 err "failed to create $HOME/.npmrc"
                 return 1
@@ -51,32 +45,23 @@ function publish() {
         else
             msg "assuming your .npmrc is setup correctly for this project"
         fi
-        if ! ( cd "$target" && npm publish --access=public ); then
-           err "failed to publish node module"
-           cat "$target/npm-debug.log"
-           return 1
-        fi
     elif [[ $TRAVIS_BRANCH == master ]]; then
-        # install latest rug dev version in package.json
-        if ! npm install @atomist/rug@latest -S --registry https://atomist.jfrog.io/atomist/api/npm/npm-dev; then
-            err "Failed to install latest @atomist/rug from https://atomist.jfrog.io/atomist/api/npm/npm-dev"
-            return 1
-        fi
-        if [[ $ATOMIST_REPO_TOKEN  && $ATOMIST_REPO_USER ]]; then
-            msg "Creating local .npmrc using API key from environment"
-            if ! ( curl -u"${ATOMIST_REPO_USER}":"${ATOMIST_REPO_TOKEN}" "https://atomist.jfrog.io/atomist/api/npm/auth" >  "$HOME/.npmrc"); then
+        if [[ $ATOMIST_REPO_TOKEN && $ATOMIST_REPO_USER ]]; then
+            msg "creating local .npmrc using API key from environment"
+            if ! ( umask 077 && curl --quiet -u"$ATOMIST_REPO_USER:$ATOMIST_REPO_TOKEN" https://atomist.jfrog.io/atomist/api/npm/auth > "$HOME/.npmrc" 2>/dev/null ); then
                 err "failed to create $HOME/.npmrc"
                 return 1
             fi
         else
             msg "assuming your .npmrc is setup correctly for this project"
         fi
+        registry=--registry=https://atomist.jfrog.io/atomist/api/npm/npm-dev-local
+    fi
 
-        if ! ( cd "$target" && npm publish --registry https://atomist.jfrog.io/atomist/api/npm/npm-dev-local --access=public ); then
-            err "failed to publish node module"
-            cat "$target/npm-debug.log"
-            return 1
-        fi
+    if ! ( cd "$target" && npm publish --access=public $registry ); then
+        err "failed to publish node module"
+        cat "$target/npm-debug.log"
+        return 1
     fi
 }
 
